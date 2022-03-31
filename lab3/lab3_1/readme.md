ed580a58c569d50d472a5cad7d80a49a216f0b0f
Raqsf <raquelsf@ua.pt>
2022-03-31 14:15:37 +0100

    Lab3_1 done

--- lab3/lab3_1/readme.md
+++ lab3/lab3_1/readme.md
@@ -1,15 +1,20 @@
 # Questions
 
 ## a) Identify a couple of examples on the use of AssertJ expressive methods chaining.
-TesteA linhas 37, 43, 52, 53, 59, 75
+We can see the use of AssertJ expressive methods in the A_EmployeeRepositoryTest in the lines 37, 43, 52, 53, 59 and 75.
 
 ## b) Identify an example in which you mock the behavior of the repository (and avoid involving a database).
-No testeB (todos), por exemplo henSearchValidName_thenEmployeeShouldBeFound()
+In B_EmployeeService_UnitTest, all of the tests mock the behavior of the repository, since they all use the employeeService which uses the employeeRepository (@Mock). The behavior of the employeeRepository is defined in the setUp method.
 
 ## c) What is the difference between standard @Mock and @MockBean?
-
+@Mock is functionally the same as Mockito.mock() and it is used for unit testing. A mock is created for the collaborator classes of the class that we are testing.
+@MockBean is used for integration testing in a specific Spring Context. This context needs some beans, that if don't provided, the context won't start. To add new beans to that context we use the @MockBean annotation.
+They both create mock objects.
 
 ## d) What is the role of the file “application-integrationtest.properties”? In which conditions will it be used?
+This file allows us to override properties from a file or use the default ones to run a test.
 
 
 ## e) The sample project demonstrates three test strategies to assess an API (C, D and E) developed with SpringBoot. Which are the main/key differences?
+In the C test (@WebMvcTest) we are just testing the EmployeeController creating a mock of the service. 
+D and E use the full Spring Boot Application (@SpringBootTest), but D uses a servlet to access the server context through the MockMvc and E uses a REST client through TestRestTemplate.
--- lab3/lab3_2/car_service/src/test/java/ua/car_service/car_service/CarControllerIT.java
+++ lab3/lab3_2/car_service/src/test/java/ua/car_service/car_service/CarControllerIT.java
@@ -1,8 +1,6 @@
 package ua.car_service.car_service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -16,15 +14,19 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 
-@AutoConfigureTestDatabase
+// Ex 2 Test E
+// @AutoConfigureTestDatabase
+
 // switch AutoConfigureTestDatabase with TestPropertySource to use a real database
-//@TestPropertySource( locations = "application-integrationtest.properties")
+
+// Ex 3
+@TestPropertySource( locations = "application-integrationtest.properties")
 public class CarControllerIT {
 
     // will need to use the server port for the invocation url
@@ -54,8 +56,13 @@ public class CarControllerIT {
 
     @Test
     void givenCars_whenGetCars_thenStatus200()  {
+        Car c1 = new Car("Nissan", "Skyline R32");
+        Car c2 = new Car("Dodge", "Trackhawk");
+        repository.save(c1);
+        repository.save(c2);
+        /* Ex3
         createTestCar("Nissan", "Skyline R32");
-        createTestCar("Dodge", "Trackhawk");
+        createTestCar("Dodge", "Trackhawk"); */
 
 
         ResponseEntity<List<Car>> response = restTemplate
@@ -71,6 +78,8 @@ public class CarControllerIT {
     void whenGetAValidCarId_thenStatus200() {
         Car car = new Car("Ford", "Raptor");
         repository.save(car);
+        // Ex 3
+        // repository.saveAndFlush(car);
 
         ResponseEntity<Car> response = restTemplate.getForEntity("/api/details/" + String.valueOf(car.getCarId()), Car.class);
 
